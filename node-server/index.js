6b11bc80e33614585cfa7e299ecd81a3fc31f33e
Richard <32272387+lauyh@users.noreply.github.com>
2022-08-16 21:43:38 +0800

    add barebone node js api

---
+++ node-server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const middlewares = require('./middlewares')
+const morgan = require('morgan')
+const {sequelize} = require('./models')
+
+require('dotenv').config()
+
+const app = express()
+app.use(morgan('dev'))
+
+
+const PORT = process.env.PORT || 3001
+
+app.get("/", (req, res)=>{
+  res.send("hello world")
+})
+
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
+
+
+app.listen(PORT, async()=> {
+  await sequelize.sync()
+
+  console.log(`Server is running on port ${PORT}`)
+})
